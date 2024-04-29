e564c0c24f139a1e4d1bf258c59d4798bd3255f3
jjuniorbrasil <juniorbrasilisc@gmail.com>
2024-04-29 15:00:13 -0300

    D.Dio

---
+++ DIO/DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,15 @@
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException() {
+    }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+    public ParametrosInvalidosException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+}
